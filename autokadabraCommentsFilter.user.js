1fc3da5221c4206e73aa3bbb9ae5c05db29c25a9
shpuntik <shpuntik74@gmail.com>
2014-01-25 20:41:28 +0600

    Add average rating, review source

--- autokadabraCommentsFilter.user.js
+++ autokadabraCommentsFilter.user.js
@@ -3,147 +3,171 @@
 // @description filter comments by rating
 // @author Alexander Ivantsov
 // @license MIT
-// @version 1.1
+// @version 1.2
 // @include http://autokadabra.ru/shouts/*
 // ==/UserScript==
 
-if ('undefined' == typeof __PAGE_SCOPE_RUN__) {
-  (function page_scope_runner(){
-    var my_src = "(" + page_scope_runner.caller.toString() + ")();";
-    var script = document.createElement('script');
+$(function(){
 
-    script.setAttribute("type", "text/javascript");
-    script.textContent = "var __PAGE_SCOPE_RUN__ = true;\n" + my_src;
+	var FilterForm = {
+		isShow: true,
+		isFilter: false,
+		form: null,
 
-    setTimeout(function() {
-          document.body.appendChild(script);
-          document.body.removeChild(script);
-        }, 0);
-  })();
+		create: function(){
+			var htmlForm = "<div id='comments-filter-form' style='background-color: #f0f0e7; position: fixed; top: 50px; right: 0; padding: 20px 40px; border: 2px solid #777;'>" +
+								"<div style='position: relative'>" +
+									"<p style='color: #000'>" +
+										"Средний рейтинг: <span style='width: 30px; color: #0072b1;'>" + Filter.getAvgRating() + "</span>" +
+									"</p>" +
+									"<p style='color: #000'>" +
+										"Мин рейтинг: <input id='comments-filter-form__inp-min-rating' style='width: 30px' type='number' placeholder='0'>" +
+									"</p>" +
+									"<button id='comments-filter-form__btn' style='margin-top: 10px'>Отфильтровать</button>" +
+								"</div>" +
+							"</div>";
 
-  return;
-};
+			$("body").append(htmlForm);
 
-function filterComments(){
-    (function($){
-        var inp = "<p style='color: #fff'>Мин рейтинг: <input id='min-comment-rating' style='width: 30px' type='text' value='0' /></p>";
-        var btn = "<button id='filter-comments' style='margin-top: 10px'>Отфильтровать камменты</button>";
-        var div = "<div class='comments_control' style='opacity: 0.5; position: fixed; top: 50px; right: 0; background-color: #2e2e2e; padding: 10px 30px 10px 10px'><div style='position: relative'>" + inp + btn + "<a id='hidePanel' href='javascript:void(0)' style='color: #fff; position: absolute; right: -25px; top: -5px; text-decoration: none; font-size: 20px; font-weight: bold; text-shadow: 0 1px 0 #FFFFFF'>×</a></div></div>";
+			this.form = $("#comments-filter-form");
+		},
 
-        $("body").append(div);
+		toggle: function(){
+			if(this.isShow){ // hide
+				this.isShow = false;
 
-        function filterComments() {
-            var minRating = parseInt($("#min-comment-rating").val());
+				this.form.animate({
+					paddingLeft: "+=80",
+					right: -1 * this.form.width() - 80
+				}, 1000);
+			}
+			else{ // show
+				this.isShow = true;
+
+                this.form.animate({
+                    paddingLeft: "-=80",
+                    right: 0
+                }, 1000);
+            };
+        },
+
+        btnClick: function(elem){
+            if(this.isFilter){
+                this.isFilter = false;
+
+                Filter.refresh();
+
+                this.form.find("p").show();
+
+                elem.html("Отфильтровать");
+            }
+            else{
+                this.isFilter = true;
+
+                Filter.run();
+
+                this.form.find("p").hide();
+
+                elem.html("Сбросить");
+            };
+        }
+	};
+
+	var Filter = {
+        getAvgRating: function(){
+            var count = 0,
+                ratingSum = 0, //sum of comments rating
+                rating;
+
+            $(".comment").each(function(){
+                rating = parseInt($(this).find(".count").html()) || 0;
+
+                if(isNaN(rating) || rating == 0) return; // skip comments with not positive rating
+
+                count++;
+                ratingSum += rating;
+            });
+
+            return Math.round(ratingSum/count);
+        },
+
+        run: function(){
+            var minRating = parseInt($("#comments-filter-form__inp-min-rating").val());
             var rating;
 
-            /* выставляем класс 'cool_comment' для каментав с нужным рейтингом */
-            $(".comments .comment").each(function () {
-                if ($(this).find(".article .vote").hasClass("has_vote")) rating = parseInt($(this).find(".article .vote .count").html());
-                else rating = 0;
+            // set class 'cool-comment' for comment with needed rating
+            $(".comments .comment").each(function(){
+                rating = $(this).find(".count").html() || 0;
 
-                if (rating >= minRating) $(this).addClass("cool_comment");
+                if( isNaN(parseInt(rating)) ) rating = -1 * parseInt(rating.replace("–", "")); // negative rating
+                else rating = parseInt(rating); // positive rating
+
+                if(rating >= minRating) $(this).addClass("cool-comment");
             });
 
-            /* 
-             * скрываем отстойные каменты
-             * добавляем кнопку, чтобы показать ответы для данного камента 
-             */
-            $(".comments .comment").each(function () {
-                if (!$(this).hasClass("cool_comment") && $(this).find(".cool_comment").length == 0) $(this).hide();
-                else if ($(this).hasClass("cool_comment") && $(this).find(".cool_comment").length == 0 && $(this).find(".reply_link .show_reply").length == 0 && $(this).find(".reply_link").length > 1) $(this).find(".reply_link").eq(0).append("<a class='show_reply' style='float: right' href='javascript:void(0)'>Показать ответы</a>");
+            // hide useless comments and add 'show comments' button for it
+            $(".comments .comment").each(function(){
+                if(!$(this).hasClass("cool-comment") && $(this).find(".cool-comment").length == 0) $(this).hide();
+                else if($(this).hasClass("cool-comment") && $(this).find(".cool-comment").length == 0 && $(this).find(".reply .show-reply").length == 0 && $(this).find(".reply_link").length > 1) $(this).find(".reply_link").eq(0).append("<a class='show-reply show-reply-hide' style='float: right' href='javascript:void(0)'>Показать ответы</a>");
             });
-        };
+        },
 
-        /* сброс фильтров */
-        function refreshComments() {
-            $(".cool_comment").each(function () {
-                $(this).removeClass("cool_comment");
-                $(this).find(".show_reply").remove();
+        refresh: function(){
+            $(".cool-comment").each(function(){
+                $(this).removeClass("cool-comment");
+                $(this).find(".show-reply").remove();
             });
 
-            $(".comments .comment").each(function () {
+            $(".comments .comment").each(function(){
                 $(this).show();
             });
-        };
+        },
 
-        var toggleBtn = 0; // 0 - фильтр, 1 - отменить фильтр
-
-        $("#filter-comments").click(function () {
-            /* запуск фильтра */
-            if (!toggleBtn) {
-                $("#filter-comments").css("margin-top", 0);
-                filterComments();
-                toggleBtn = 1;
-                $("#min-comment-rating").parent().slideUp();
-                $(this).html("Сбросить фильтр");
-            }
-            /* сбросить фильтр */
-            else {
-                $("#filter-comments").css("margin-top", "10px");
-                refreshComments();
-                toggleBtn = 0;
-                $("#min-comment-rating").parent().slideDown();
-                $(this).html("Отфильтровать камменты");
-            };
-        });
-
-        var togglePanel = 0; // 0 - открыто, 1 - свернуто
-
-        /* скрыть панель */
-        $("#hidePanel").click(function (event) {
-            event.stopPropagation();
-
-            togglePanel = 1;
-            $(".comments_control").animate({
-                paddingLeft: "+=20",
-                right: -$(".comments_control").width() - 30
-            }, 1000);
-        });
-
-        /* открыть панель */
-        $(".comments_control").click(function () {
-            if (togglePanel == 0) return 0;
-            togglePanel = 0;
-            $(this).animate({
-                paddingLeft: "-=20",
-                right: 0
-            }, 1000);
-        });
-
-        /* управление прозрачностью панели */
-        $(".comments_control").hover(function () {
-            $(this).css("opacity", 1);
-        }, function () {
-            $(this).css("opacity", 0.5);
-        });
-
-
-        /* показать скрытые ответы */
-        $(document).on("click", ".show_reply", function () {
+        toggleComments: function(elem){
             var parent;
 
-            /* если ответы скрыты */
-            if ($(this).html() == "Показать ответы") {
-                $(this).html("Скрыть ответы");
+            if(elem.hasClass("show-reply-hide")){
+                elem.removeClass("show-reply-hide");
+                elem.html("Скрыть ответы");
 
-                parent = $(this).closest(".comment");
+                parent = elem.closest(".comment");
 
-                parent.find(".comment").each(function () {
+                parent.find(".comment").each(function(){
                     $(this).show();
                 });
             }
-            /* если ответы открыты */
-            else {
-                $(this).html("Показать ответы");
+            else{
+                elem.addClass("show-reply-hide");
+                elem.html("Показать ответы");
 
-                parent = $(this).closest(".comment");
+                parent = elem.closest(".comment");
 
-                parent.find(".comment").each(function () {
-                    if (!$(this).hasClass("cool_comment") && $(this).find(".cool_comment").length == 0) $(this).hide();
+                parent.find(".comment").each(function(){
+                    if(!$(this).hasClass("cool-comment") && $(this).find(".cool-comment").length == 0) $(this).hide();
                 });
-            }
-        });
-    })(jQuery);
-};
+            };
+        }
+    };
 
-filterComments();
+	FilterForm.create();
+
+    $("body").on("click", "#comments-filter-form__inp-min-rating", function(){
+        return false;
+    });
+
+    $("body").on("click", "#comments-filter-form__btn", function(){
+        FilterForm.btnClick($(this));
+
+        return false;
+    });
+
+    // show and close comments filter form
+    $("body").on("click", "#comments-filter-form", function(){
+        FilterForm.toggle();
+    });
+
+    $("body").on("click", ".show-reply", function(){
+        Filter.toggleComments($(this));
+    });
+
+})
